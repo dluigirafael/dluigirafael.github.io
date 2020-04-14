c9008124b37a331a4f4fdbcb3be53eaf378721f5
dluigirafael <37952719+dluigirafael@users.noreply.github.com>
2020-04-14 16:57:39 +0300

    Update scripts.js

--- ubuntu20.04/scripts.js
+++ ubuntu20.04/scripts.js
@@ -2,9 +2,22 @@ function border(id) {
 
     if (document.getElementById(id).style.borderBottom === "") {
         document.getElementById(id).style.borderBottom = "solid aqua 2px"
-    }
-    else{
-    document.getElementById(id).style.borderBottom = "";
+    } else {
+        document.getElementById(id).style.borderBottom = "";
     }
 }
 
+ function clock() {
+
+    let today = new Date();
+    let date = today.getDate();
+    let months = ["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"];
+    let month = months[today.getMonth()]
+    let hour = today.getHours();
+    let minute= today.getMinutes();
+    if (minute < 10) {minute = "0" + i};
+
+    document.getElementById('clock').innerHTML = ` ${month} ${date}&nbsp;&nbsp; ${hour}:${minute}`;
+    setTimeout(clock, 30000);
+
+}
